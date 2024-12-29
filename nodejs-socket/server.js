0f68a96a14c6634d1fa4f408ae92ea202b422dfd
Arunvasuacchu <arunvasur@gmail.com>
2024-12-29 18:45:05 +0530

    socket wit node js

---
+++ nodejs-socket/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const http = require('http')
+const socketIO = require('socket.io')
+
+const server = http.createServer(app)
+const io = socketIO(server);
+app.use(express.static('public'))
+
+const users = new Set();
+
+io.on('connection', (socket) => {
+  console.log('user is connected');
+
+  // handle user if they connected
+
+  socket.on('join', (userName) => {
+    users.add(userName)
+  })
+  // broadcasting the user
+
+  io.emit('userJoined',userName)
+
+  io.emit('userList', Array.from(users))
+})
