cdf3f47ad61e7f82c846550715a6241e98ffc48e
Arunvasuacchu <arunvasur@gmail.com>
2025-01-22 19:48:19 +0530

    advance node js

---
+++ advance-node-js/event-loop.js
@@ -0,0 +1,13 @@
+// what is event loop? 
+// event loop is a js mechanism that enables non blocking io operations and makes programing responsoive
+// microtask, macrotask, check, IO callbacks, cpu intensive operations --> this is the order this is how program runs.
+
+setTimeout(() => {
+  console.log('set timeout')
+}, 0)
+
+setImmediate(() => {
+  // run something
+  console.log('imm');
+  
+});
