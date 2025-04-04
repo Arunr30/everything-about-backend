b9c1e56ccd36ee2338fe22c97f83890ff59fddcf
Arunvasuacchu <arunvasur@gmail.com>
2025-04-04 10:24:37 +0530

    revised some express concepts

--- smolljames/chapter-2/server.js
+++ smolljames/chapter-2/server.js
@@ -2,7 +2,21 @@ import express from "express"
 const app =  express();
 
 const PORT = 3001;
+let user = [];
+app.use(express.json());
 
+app.post('/new-data', (req, res) => {
+  const name = req.body
+  user.push(name);
+})
+
+app.get('/data', (req, res) => {
+  res.send(user)
+})
+
+app.delete('/delete', (req, res) => {
+  user.splice(0, 1);
+})
 app.get('/', (req, res) => {
   res.send('<h1>this is html code</h1>')
 })
